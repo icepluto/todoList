29d17ebc4ee3071920ffe82874f40d0e689a325b
icepluto <18792689010@163.com>
2019-05-31 18:56:08 +0800

    error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './app.vue'
 
+import './assets/styles/test.css'
+import './assets/styles/test-stylus.styl'
+import './assets/images/3361f3a540b3707d6bf9c8018c0f0a79.jpg'
+
 const root = document.createElement('div')
 document.body.appendChild(root)
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,85 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development'
+const config = {
+    target: 'web',
     entry: path.join(__dirname,'src/index.js'),
     output:{
         filename: 'bundle.js',
         path:path.join(__dirname,'dist')
     },
-    plugins: [
-        new VueLoaderPlugin()
-    ],
     module:{
         rules:[
             {
-                test:/.vue$/,
+                test:/\.vue$/,
                 loader:'vue-loader'
+            },
+            {
+                test:/\.jsx$/,
+                loader:'babel-loader'
+            },
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.styl/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            sourceMap:true,
+                        }
+                    },
+                    'stylus-loader'
+                ]
+            },
+            {
+                test:/\.(gif|jpg|png|jpeg|svg)$/,
+                use:[
+                    {
+                        loader:'url-loader',
+                        options:{
+                            limit:1024,
+                            name:'[name]-aaa.[ext]'
+                        }
+                    }
+                ]
             }
         ]
     },
-    mode: 'development' 
-};
+    mode: 'development' ,
+    plugins:[
+        new VueLoaderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env':{
+                NODE_ENV:isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin()
+    ]
+}
+if(isDev) {
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay:{
+            errors: true,
+        },
+        hot:true,
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin()
+    )
+}
+
+module.exports = config
